09b5ca0fb76766b5c0c490eab1cecf587f511611
Muhammaddiyor <muhammaddiyorashurov5@gmail.com>
2023-04-27 05:44:03 +0300

    add serializers.py file

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import News, Category
 
-admin.site.registrer(News)
-admin.site.registrer(Category)
+admin.site.register(News)
+admin.site.register(Category)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework import generics
+from .models import News
+from .serializers import NewsSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+# class NewsAPIView(generics.ListAPIView):
+#     queryset = News.objects.all()
+#     serializer_class = NewsSerializer
+
+class NewsAPIView(APIView):
+    def get(self, request):
+        lst = News.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = News.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+        return Response({"post": model_to_dict(post_new)})
